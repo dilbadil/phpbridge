df9aad5e0b946bf428ef6ed2857eb4db5fdf84e1
dilbadil <abdillah308@gmail.com>
2015-02-16 08:47:56 +0700

    edit structure

--- public/index.php
+++ public/index.php
@@ -1,11 +1,7 @@
 <?php 
 
 require_once '../vendor/autoload.php';
-// require_once '../src/Suggestotron/Config.php';
 \Suggestotron\Config::setDirectory('../config');
-
-// $config = \Suggestotron\Config::get('autoload');
-// require_once $config['class_path'] . '/Suggestotron/Autoloader.php';
 
 $route = null;
 
